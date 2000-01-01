4d446fba919e097df50b0f21d128e9b8930e3113
tubbleWeek <wchastek@gmail.com>
1999-12-31 20:22:59 -0600

    added pure_pursuit nodes and updated usage

--- src/f1tenth_controllers/setup.py
+++ src/f1tenth_controllers/setup.py
@@ -1,4 +1,6 @@
 from setuptools import setup
+import os
+from glob import glob
 
 package_name = 'f1tenth_controllers'
 
@@ -10,6 +12,7 @@ setup(
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
+        (os.path.join('share', package_name, 'launch'), glob('launch/*.py')),
     ],
     install_requires=['setuptools'],
     zip_safe=True,
@@ -21,7 +24,8 @@ setup(
     entry_points={
         'console_scripts': [
             'follow_the_gap_node = f1tenth_controllers.follow_the_gap_node:main',
-            'pure_pursuit_node = f1tenth_controllers.pure_pursuit:main'
+            'pure_pursuit_node = f1tenth_controllers.pure_pursuit:main',
+            'pure_pursuit_nodev2 = f1tenth_controllers.pure_pursuit_sim:main'
         ],
     },
 )
