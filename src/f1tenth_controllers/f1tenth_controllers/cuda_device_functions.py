5c84aefa1304abed0f41eb992b2f366dc38c4ea3
tubbleWeek <wchastek@gmail.com>
2025-01-21 13:23:56 -0600

    (Yukang)Add mppi initial controller node, setup ros2 structure and missing dependencies on Jetson

---
+++ src/f1tenth_controllers/f1tenth_controllers/cuda_device_functions.py
@@ -0,0 +1,147 @@
+import numpy as np
+import math
+import copy
+import numba
+import time
+from numba import cuda
+from numba.cuda.random import create_xoroshiro128p_states, xoroshiro128p_normal_float32
+import matplotlib.pyplot as plt
+import pdb
+
+
+
+# Stage costs (device function)
+@cuda.jit('float32(float32, float32)', device=True, inline=True)
+def stage_cost(dist2, dist_weight):
+    return dist_weight*dist2 # squared term makes the robot move faster
+
+# Terminal costs (device function)
+@cuda.jit('float32(float32, boolean)', device=True, inline=True)
+def term_cost(dist2, goal_reached):
+    return (1-np.float32(goal_reached))*dist2
+
+@cuda.jit('void(float32[:], float32, float32, float32[:,:])', device=True, inline=True)
+def get_vehicle_boundary_points(x_curr, vehicle_length, vehicle_width, vehicle_boundary_points):
+    x_center = x_curr[0]
+    y_center = x_curr[1]
+    theta = x_curr[2]
+    # epsilon = 0.1 outer bound for the car
+    half_length = (vehicle_length / 2) + 0.1
+    half_width = (vehicle_width / 2) + 0.1
+
+    # Define relative corner positions
+    corners = cuda.local.array((6, 2), dtype=np.float32)
+    corners[0, 0], corners[0, 1] = half_length, half_width    # Front left
+    corners[1, 0], corners[1, 1] = half_length, -half_width   # Front right
+    corners[2, 0], corners[2, 1] = 0.0, -half_width   # center right
+    corners[3, 0], corners[3, 1] = -half_length, -half_width  # Rear right
+    corners[4, 0], corners[4, 1] = -half_length, half_width   # Rear left
+    corners[5, 0], corners[5, 1] = 0.0, half_width   # Center left
+    '''
+        corners = np.array([
+        [half_length, half_width],     # Front left
+        [half_length, -half_width],    # Front right
+        [0, -half_width],               # center right,   
+        [-half_length, -half_width],   # Rear right
+        [-half_length, half_width],    # Rear left,
+        [0, half_width]    # center left
+    ])'''
+    # Precompute trigonometric functions for rotation
+    cos_theta = math.cos(theta)
+    sin_theta = math.sin(theta)
+
+    # Rotate each corner using the vehicle's heading, then translate to global coordinates
+    for i in range(6):
+        rotated_x = corners[i, 0] * cos_theta - corners[i, 1] * sin_theta
+        rotated_y = corners[i, 0] * sin_theta + corners[i, 1] * cos_theta
+
+        vehicle_boundary_points[i, 0] = rotated_x + x_center
+        vehicle_boundary_points[i, 1] = rotated_y + y_center
+
+
+#Convert the vehicle boundary points to the grid coordinates
+@cuda.jit('void(float32[:,:], float32, float32, float32, float32, float32[:,:])', device=True, inline=True)
+def get_vehicle_boundary_points_grid(vehicle_boundary_points, x_min, y_min, grid_resolution,scale, vehicle_boundary_points_grid):
+    for i in range(4):
+        x = vehicle_boundary_points[i, 0]
+        y = vehicle_boundary_points[i, 1]
+
+        x_grid = int(((scale * x - x_min)) / grid_resolution)
+        y_grid = int(((scale * y - y_min)) / grid_resolution)
+
+        vehicle_boundary_points_grid[i, 0] = x_grid
+        vehicle_boundary_points_grid[i, 1] = y_grid
+
+@cuda.jit('float32(float32[:,:], float32, float32, float32[:,:])', device=True, inline=True)
+def calculate_obstacle_cost(vehicle_boundary_points_grid, obstacle_weight, max_cost, costmap):
+    cost = 0.0
+    for i in range(4):
+        corner_x = vehicle_boundary_points_grid[i, 0]
+        corner_y = vehicle_boundary_points_grid[i, 1]
+        cost += obstacle_weight*(max_cost -costmap[int(corner_y), int(corner_x)])
+    return cost
+
+
+
+# @cuda.jit('float32(float32[:,:], float32[:,:], float32[:], float32)', device=True, inline=True)
+# def calculate_obstacle_cost(vehicle_boundary_points, obs_pos_d, obs_r_d, obs_cost_d):
+#     num_obs = len(obs_pos_d)
+#     cost = 0.0
+
+#     for obs_i in range(num_obs):
+#         obs_x = obs_pos_d[obs_i, 0]
+#         obs_y = obs_pos_d[obs_i, 1]
+#         obs_radius_sq = obs_r_d[obs_i]
+
+#         for i in range(4):
+#             corner_x = vehicle_boundary_points[i, 0]
+#             corner_y = vehicle_boundary_points[i, 1]
+#             dist_sq = (corner_x - obs_x) ** 2 + (corner_y - obs_y) ** 2 - obs_radius_sq**2
+#             cost += (1-numba.float32(dist_sq>0))*obs_cost_d
+
+#     return cost
+
+
+@cuda.jit('float32(float32, float32, float32, float32,float32, float32)', device=True, inline=True)
+def steering_contrainsts_cuda(steering_angle, steering_velocity, min_angle, max_angle, v_min, v_max):
+
+    # Check if steering limit is reached
+    if (steering_angle <= min_angle and steering_velocity <= 0) or (steering_angle >= max_angle and steering_velocity >= 0):
+        steering_velocity = 0
+    elif steering_velocity <= v_min:
+        steering_velocity = v_min
+    elif steering_velocity >= v_max:
+        steering_velocity = v_max
+
+    return steering_velocity
+
+
+
+@cuda.jit('float32(float32, float32, float32, float32, float32, float32)', device=True, inline=True)
+def acceleration_contrainsts_cuda(velocity, acceleration, v_switch, a_max, v_min, v_max):
+
+    # positive acceleration limit
+    if velocity > v_switch:
+        posLimit = a_max * v_switch / velocity
+    else:
+        posLimit = a_max
+
+    # acceleration limit reached?
+    if (velocity <= v_min and acceleration <= 0) or (velocity >= v_max and acceleration >= 0):
+        acceleration = 0
+    elif acceleration <= -a_max:
+        acceleration = -a_max
+    elif acceleration >= posLimit:
+        acceleration = posLimit
+
+    return acceleration
+
+# Define angle wrapper in cuda device function for yaw angle bbetween -pi and pi
+@cuda.jit('float32(float32)', device=True, inline=True)
+def angle_wrap_cuda(angle):
+    return (angle + np.pi) % (2 * np.pi) - np.pi
+
+# Define angle wrapper in cuda device function for yaw angle between 0 and 2pi
+@cuda.jit('float32(float32)', device=True, inline=True)
+def angle_wrap_2pi_cuda(angle):
+    return angle % (2 * np.pi)
---
+++ src/f1tenth_controllers/f1tenth_controllers/input_constraints.py
@@ -0,0 +1,109 @@
+import numpy as np
+import math
+def acceleration_constraints(velocity, acceleration, v_min, v_max, a_max, v_switch):
+    """
+    accelerationConstraints - adjusts the acceleration based on acceleration constraints
+
+    Inputs:
+        :param acceleration - acceleration in driving direction
+        :param velocity - velocity in driving direction
+        :params p - longitudinal parameter structure
+
+    Outputs:
+        :return acceleration - acceleration in driving direction
+
+    Author: Matthias Althoff
+    Written: 15-December-2017
+    Last update: ---
+    Last revision: ---
+    """
+    # positive acceleration limit
+    if velocity > v_switch:
+        posLimit = a_max * v_switch / velocity
+    else:
+        posLimit = a_max
+
+    # acceleration limit reached?
+    if (velocity <= v_min and acceleration <= 0) or (velocity >= v_max and acceleration >= 0):
+        acceleration = 0
+    elif acceleration <= -a_max:
+        acceleration = -a_max
+    elif acceleration >= posLimit:
+        acceleration = posLimit
+
+    return acceleration
+
+def steering_constraints(steering_angle, steering_velocity, min_angle, max_angle, v_min, v_max):
+    """
+    steering_constraints - adjusts the steering velocity based on steering
+
+    Inputs:
+        :param steering_angle - steering angle
+        :param steering_velocity - steering velocity
+        :params p - steering parameter structure
+
+
+
+    Outputs:
+        :return steering_velocity - steering velocity
+
+    Author: Matthias Althoff
+    Written: 15-December-2017
+    Last update: ---
+    Last revision: ---
+    """
+    # steering limit reached?
+    if (steering_angle <= min_angle and steering_velocity <= 0) or (steering_angle >= max_angle and steering_velocity >= 0):
+        steering_velocity = 0
+    elif steering_velocity <= v_min:
+        steering_velocity = v_min
+    elif steering_velocity >= v_max:
+        steering_velocity = v_max
+
+    return steering_velocity
+
+# Define angle wrapper for yaw angle
+def angle_wrap(angle):
+    """
+    Wrap angle to [-pi, pi].
+
+    Parameters:
+    - angle: Angle in radians.
+
+    Returns:
+    - wrapped_angle: Wrapped angle in radians.
+    """
+    wrapped_angle = (angle + np.pi) % (2 * np.pi) - np.pi
+    return wrapped_angle
+
+# Define angle wrapper for yaw angle between [0, 2*pi]
+def angle_wrap_2pi(angle):
+    """
+    Wrap angle to [0, 2*pi].
+
+    Parameters:
+    - angle: Angle in radians.
+
+    Returns:
+    - wrapped_angle: Wrapped angle in radians.
+    """
+    wrapped_angle = angle % (2 * np.pi)
+    return wrapped_angle
+
+
+def convert_position_to_costmap_indices_cpu(position, map_resolution=0.05, origin=[-15, -10]):
+    # 10 is the map scaling factor from the costmap generation
+    map_y = int((10 * position[0] - origin[0]) / map_resolution)
+    map_x = int((10 * position[1] - origin[1] ) / map_resolution)
+    return map_x, map_y
+
+
+''' @brief: Returning the mean and standard deviation of the lognormal distribution, 
+           given mean and variance of Normal distribution'''
+def Normal2LogN(m, v):
+    """ m: mean, v: variance
+    Return: mu: mean, sigma: standard deviation of LN dist"""
+    mu = np.exp(m + 0.5 * v)
+    var = np.exp(2 * m + v) * (np.exp(v) - 1)
+    sigma = np.sqrt(var)
+    return mu, sigma
